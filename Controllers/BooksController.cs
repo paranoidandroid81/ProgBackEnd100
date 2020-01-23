367eb66332e2706b6ee6b349998717c51b9e7fc9
Mikey Korst <mpk44@pitt.edu>
2020-01-23 14:44:05 +0000

    Added ability to add books through API

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -16,7 +16,46 @@ namespace LibraryApi.Controllers
             _libContext = libContext;
         }
 
-        [HttpGet("/books/{id:int}")]
+        [HttpPost("/books")]
+        public async Task<IActionResult> AddABook([FromBody] PostBooksRequest bookToAdd)
+        {
+            // Validate it. (if invalid, return a 400 Bad Request)
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // Don't use imperative validation, use field validation in model class
+            // Add it to the domain.
+            // - PostBooksRequest -> Books
+            var book = new Book
+            {
+                Title = bookToAdd.Title,
+                Author = bookToAdd.Author,
+                Genre = bookToAdd.Genre,
+                NumberOfPages = bookToAdd.NumberOfPages,
+                InInventory = true
+            };
+            // - Add it to the context
+            _libContext.Books.Add(book);
+            // - Have the context save everything.
+            await _libContext.SaveChangesAsync(); // this also creates an ID for the book
+            // Return a 201 Created Status Code.
+            // - Add a location header on the response, e.g. Location: http://server/books/8
+            // - Add the entity
+            // - Book -> GetBooksDetailResponse
+
+            var response = new GetBookDetailsResponse
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Author = book.Author,
+                Genre = book.Genre,
+                NumberOfPages = book.NumberOfPages
+            };
+            return CreatedAtRoute("books#getbookbyid", new {  id = response.Id }, response);
+        }
+
+        [HttpGet("/books/{id:int}", Name = "books#getbookbyid")]
         public async Task<IActionResult> GetBookById(int id)
         {
             var resp = await _libContext.Books
---
+++ Models/PostBooksRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models
+{
+    public class PostBooksRequest : IValidatableObject
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Author { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Range(1, int.MaxValue)]
+        public int NumberOfPages { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == "Great Expectations" && Author == "Dickens")
+            {
+                yield return new ValidationResult("Alec hates that book!", new string[] { "Title", "Author" });
+            }
+        }
+    }
+}
